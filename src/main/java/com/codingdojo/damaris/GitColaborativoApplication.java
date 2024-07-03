7852144cfd820682383cb31deb511d04eec6c4e8
Damaris <calamitydam@gmail.com>
2024-07-03 19:48:57 -0300

    Inicio Proyecto!

---
+++ src/main/java/com/codingdojo/damaris/GitColaborativoApplication.java
@@ -0,0 +1,13 @@
+package com.codingdojo.damaris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitColaborativoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitColaborativoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codingdojo/damaris/GitColaborativoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.damaris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitColaborativoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
